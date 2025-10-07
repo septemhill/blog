287470eb8775e7a2f1b67c7a97209706bb0a1254
Septem Li <1216949+septemhill@users.noreply.github.com>
2025-10-07 13:57:54 +0800

    update deploy script and config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: 'export',
+  basePath: isGitHubPages ? '/blog' : '',
+  assetPrefix: isGitHubPages ? '/blog/' : '',
 };
 
 export default nextConfig;
